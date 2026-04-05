318cd24df16cfdab7d10968ee1f2c2258efccc38
daiminhdzchimto-arch <daiminhdzchimto-arch@users.noreply.github.com>
2026-04-05 11:05:47 +0000

    deploy: c2068afcc622ecdcc0329c87071fcfa26ec3df8f

---
+++ previews/pr-335/commitlint.config.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      1,
+      "always",
+      [
+        "build",
+        "chore",
+        "ci",
+        "docs",
+        "feat",
+        "fix",
+        "perf",
+        "refactor",
+        "revert",
+        "style",
+        "test",
+        "release",
+      ],
+    ],
+    "subject-empty": [1, "always"],
+    "type-empty": [1, "always"],
+  },
+};
